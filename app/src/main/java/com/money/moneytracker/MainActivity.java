82aef2203a756311e9dffdb1663a518561bbce7d
GeorgeKalyaev <gerakalyaev@mail.ru>
2017-10-29 14:00:13 +0300

    Change textSize, textColor, background, override method

--- app/src/main/java/com/money/moneytracker/MainActivity.java
+++ app/src/main/java/com/money/moneytracker/MainActivity.java
@@ -14,26 +14,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onStart(){
         super.onStart();
     }
-
-
     @Override
     protected void onResume(){
         super.onResume();
     }
-
-
     @Override
     protected void onPause(){
         super.onPause();
     }
-
-
     @Override
     protected void onStop(){
         super.onStop();
     }
-
-
     @Override
     protected void onDestroy(){
         super.onDestroy();
